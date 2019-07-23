6c6af71cd0e19d1cca6d8f7edf78a4b2078db59d
Ауесхан Малика Асылханкызы <@corp.itstep>
2019-07-23 19:23:55 +0600

    Добавьте файлы проекта.

---
+++ ProxyPattern/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ProxyPattern
+{
+    class MainApp
+    {
+
+        static void Main()
+        {
+
+            MathProxy proxy = new MathProxy();
+
+
+            Console.WriteLine("10 + 2 = " + proxy.Add(4, 2));
+            Console.WriteLine("10 - 2 = " + proxy.Sub(4, 2));
+            Console.WriteLine("10 * 2 = " + proxy.Mul(4, 2));
+            Console.WriteLine("10 / 2 = " + proxy.Div(4, 2));
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface IMath
+    {
+        double Add(double x, double y);
+        double Sub(double x, double y);
+        double Mul(double x, double y);
+        double Div(double x, double y);
+
+    }
+
+    class Math : IMath
+    {
+        public double Add(double x, double y) { return x + y; }
+        public double Sub(double x, double y) { return x - y; }
+        public double Mul(double x, double y) { return x * y; }
+        public double Div(double x, double y) { return x / y; }
+
+    }
+    class MathProxy : IMath
+    {
+        private Math _math = new Math();
+
+        public double Add(double x, double y)
+        {
+            return _math.Add(x, y);
+        }
+
+        public double Sub(double x, double y)
+        {
+            return _math.Sub(x, y);
+        }
+
+        public double Mul(double x, double y)
+        {
+            return _math.Mul(x, y);
+        }
+
+        public double Div(double x, double y)
+        {
+            return _math.Div(x, y);
+        }
+    }
+}
